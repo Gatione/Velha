9e88e486724f1e0dba605c37c6f14125d0955e39
Gatione <willianx64@gmail.com>
2022-03-10 23:35:15 -0300

    redução do tamanho das letras e adição do setTimeout

--- script.js
+++ script.js
@@ -62,47 +62,49 @@ function selectCell(x, y){
     let playerAtual = countTurn % 2 === 0 ? 'X' : 'O'
     celulaAtual.innerText = playerAtual
     
-
-    if (cells[1][1] !== 0){
-        if (cells[0][0] === cells[1][1] && cells[2][2] === cells[1][1]){
-            return final(playerAtual)
+    
+    setTimeout(() => {
+        if (cells[1][1] !== 0){
+            if (cells[0][0] === cells[1][1] && cells[2][2] === cells[1][1]){
+                return final(playerAtual)
+            }
+            if (cells[0][2] === cells[1][1] && cells[2][0] === cells[1][1]){
+                return final(playerAtual)
+            }
+            if (cells[0][1] === cells[1][1] && cells[2][1] === cells[1][1]){
+                return final(playerAtual)
+            }
+            if (cells[1][0] === cells[1][1] && cells[1][2] === cells[1][1]){
+                return final(playerAtual)
+            }
         }
-        if (cells[0][2] === cells[1][1] && cells[2][0] === cells[1][1]){
-            return final(playerAtual)
+        if(cells[0][0] !== 0){
+            if (cells[0][1] === cells[0][0] && cells[0][2] === cells[0][0]){
+                return final(playerAtual)
+            }
+            if (cells[1][0] === cells[0][0] && cells[2][0] === cells[0][0]){
+                return final(playerAtual)
+            }
         }
-        if (cells[0][1] === cells[1][1] && cells[2][1] === cells[1][1]){
-            return final(playerAtual)
+        if(cells[2][2] !== 0){
+            if (cells[0][2] === cells[2][2] && cells[1][2] === cells[2][2]){
+                return final(playerAtual)
+            }
+            if (cells[2][0] === cells[2][2] && cells[2][1] === cells[2][2]){
+                return final(playerAtual)
+            }
         }
-        if (cells[1][0] === cells[1][1] && cells[1][2] === cells[1][1]){
-            return final(playerAtual)
+        let vazio = 0
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].includes(0) ? vazio : vazio++
+            
+            if(vazio === 3){
+                alert('Empate')
+                resetAll()
+            }
         }
-    }
-    if(cells[0][0] !== 0){
-        if (cells[0][1] === cells[0][0] && cells[0][2] === cells[0][0]){
-            return final(playerAtual)
-        }
-        if (cells[1][0] === cells[0][0] && cells[2][0] === cells[0][0]){
-            return final(playerAtual)
-        }
-    }
-    if(cells[2][2] !== 0){
-        if (cells[0][2] === cells[2][2] && cells[1][2] === cells[2][2]){
-            return final(playerAtual)
-        }
-        if (cells[2][0] === cells[2][2] && cells[2][1] === cells[2][2]){
-            return final(playerAtual)
-        }
-    }
-    let vazio = 0
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].includes(0) ? vazio : vazio++
-        console.log(vazio)
-        if(vazio === 3){
-            alert('Empate')
-            resetAll()
-        }
-    }
-    console.table(cells)
+        console.table(cells)
+    }, 200)
     }
     return cells
 }
